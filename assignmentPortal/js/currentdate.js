0b64e6d4ead673dedc20e351db8164686996c6d7
JDeVoldemort <dragonlore8@gmail.com>
2022-04-22 21:17:30 -0600

    commit portal

---
+++ assignmentPortal/js/currentdate.js
@@ -0,0 +1,19 @@
+// document.getElementById('currentdate').textContent = fulldate;
+try {
+  const todaysdate = new Date();
+  const year = todaysdate.getFullYear();
+  let options = {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hc: "h24",
+  };
+  document.querySelector("#currentyear").textContent = year;
+  document.getElementById("currentdate").textContent =
+    new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+  alert("error Browser does not support locale");
+}
---
+++ datetest.js
@@ -0,0 +1,7 @@
+const daycount = '54';
+const targetEvent = Christmas;
+const dayUntil = "There are " + daycount + " days until " + targetEvent + "!";
+
+const daysUntil = `There are ${daycount} days until ${targetEvent}!`;
+
+console.log(daysUntil);
