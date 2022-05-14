e6aab3618c60a271791565abf7260a860cf80c4b
JDeVoldemort <dragonlore8@gmail.com>
2022-05-13 22:55:19 -0600

    getting there

---
+++ chamber/js/scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+
+
+    document.querySelector('#menubtn').classList.toggle('open')
+    document.querySelector('#navigate').classList.toggle('open');
+}
+// const y = document.querySelector('#menubtn')
+
+document.querySelector('#menubtn').addEventListener('click', toggleMenu)
