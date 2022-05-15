2fc8c9436eca5104ef799ea0acfdbd16a299ce34
JDeVoldemort <dragonlore8@gmail.com>
2022-05-15 15:52:38 -0600

    change background color

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -1,8 +1,16 @@
 // function toggleMenu() {
-  const menubtn = document.querySelector(".menubtn");
-  const navigate = document.querySelector(".navigate")
+const menubtn = document.querySelector(".menubtn");
+const navigate = document.querySelector(".navigate");
 // }
 // const y = document.querySelector('#menubtn')
 
-menubtn.addEventListener("click", () => {navigate.classList.toggle('respond')}, false);
-window.onesize = () => {if (window.innerWidth >891) navigate.classList.remove('respond')};
+menubtn.addEventListener(
+  "click",
+  () => {
+    navigate.classList.toggle("respond");
+  },
+  false
+);
+window.onesize = () => {
+  if (window.innerWidth > 891) navigate.classList.remove("respond");
+};
