1cefc29629b000b3169990b80e81a71d6a55d155
JDeVoldemort <dragonlore8@gmail.com>
2022-05-30 15:42:09 -0600

    round windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -5,13 +5,15 @@ console.log(temper);
 console.log(windspu);
 console.log(windchu);
 if (temper <= 50 && windspu > 3) {
-  const f =
+  let f =
     35.74 +
    (0.6215 * temper) -
     (35.75 * Math.pow(windspu, 0.16)) +
     (0.4275 * temper * Math.pow(windspu, 0.16));
   console.log(f);
-  windchu.textContent = f;
+  f = f.toPrecision(3);
+  console.log(f);
+  windchu.textContent = `${f}℃`;
 } else {
   f = "N/A";
   windchu.textContent = f;
