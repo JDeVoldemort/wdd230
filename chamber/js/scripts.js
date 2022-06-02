89e8468d14039e5216e124d16fe4e868d8b78c0b
JDeVoldemort <dragonlore8@gmail.com>
2022-06-01 19:21:44 -0600

    fixes

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -25,9 +25,10 @@ window.twosize = () => {
 }
 const current = new Date();
 const inviteBanner = document.querySelector('.invitebanner');
-  if (current.getDay() == 3 || current.getDay() == 2){
+  if (current.getDay() == 2 || current.getDay() == 1){
     inviteBanner.style.display = "block";
     inviteBanner.style.padding = "1em";
+    inviteBanner.style.margin = "0em";
 
   } else {
     inviteBanner.style.display = "none";
