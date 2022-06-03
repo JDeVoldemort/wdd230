f4413bd132ed14bcb7e96e34fa5e244d3d09bc9f
JDeVoldemort <dragonlore8@gmail.com>
2022-06-02 23:29:07 -0600

    content discover add

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -17,12 +17,12 @@ window.onesize = () => {
 // window.addEventListener 
 const heroImg = document.querySelector('#heroimg');
 
-window.twosize = () => {
-  if (window.innerWidth > 891) 
-    heroImg.removeAttribute('src');
-    heroImg.setAttribute('src', 'images/hero.jpg');
+// window.twosize = () => {
+//   if (window.innerWidth > 891) 
+//     heroImg.removeAttribute('src');
+//     heroImg.setAttribute('src', 'images/hero.jpg');
   
-}
+// }
 const current = new Date();
 const inviteBanner = document.querySelector('.invitebanner');
   if (current.getDay() == 2 || current.getDay() == 1){
