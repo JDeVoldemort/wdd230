47b36a3197c1a60378c457693f164871ca77f8a9
JDeVoldemort <dragonlore8@gmail.com>
2022-06-03 18:41:37 -0600

    formmatting fixed

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,29 +1,31 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
-    threshold: .4,
-    rootMargin: "0px 0px 50px, 0px"
+  threshold: 0.4,
+  rootMargin: "0px 0px 50px, 0px",
 };
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {image.removeAttribute("data-src");};
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
 };
 
 if ("intersectionObserver" in window) {
-    const imgObserver = new intersectionObserver((items, imgObserver) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target)
-            }
-        })
-    }, imgOptions);
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
+  const imgObserver = new intersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
     });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
 } else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
