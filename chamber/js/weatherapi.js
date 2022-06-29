9838151620f7e5a7e42e0ecdaee217d9849c6581
JDeVoldemort <dragonlore8@gmail.com>
2022-06-28 18:03:23 -0600

    reduce css impact fix links

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,7 +3,26 @@ const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#weathericon');
 const windspv= document.querySelector('#windspv');
 // const captionDesc = document.querySelector('figcaption');
-
+// const temper = parseInt(document.querySelector("#temp").innerHTML);
+// const windspu = parseFloat(document.querySelector("#windspv").innerHTML);
+const windchu = document.querySelector("#windchu");
+// console.log(temper);
+// console.log(windspu);
+// console.log(windchu);
+if (currentTemp <= 50 && windspv > 3) {
+  let f =
+    35.74 +
+   (0.6215 * currentTemp) -
+    (35.75 * Math.pow(windspu, 0.16)) +
+    (0.4275 * currentTemp * Math.pow(windspu, 0.16));
+  console.log(f);
+  f = f.toPrecision(3);
+  console.log(f);
+  windchu.textContent = `${f}℉`;
+} else {
+  f = "N/A";
+  windchu.textContent = f;
+}
 async function apiFetch() {
     try {
       const response = await fetch(weathURL);
