d2013b52c940bb1a8274fe385a9c1e8db3592eb3
JDeVoldemort <dragonlore8@gmail.com>
2022-07-18 18:29:42 -0600

    nav upgrade for services sub pages

--- templehotel/js/menu.js
+++ templehotel/js/menu.js
@@ -1,11 +1,31 @@
 const menubtn = document.querySelector(".menubtn");
 const navigate = document.querySelector(".navigate");
-
+const subnav = document.querySelector(".subnav");
+const subnavC1 = document.querySelector(".subnav-content1");
+const subnavC2 = document.querySelector(".subnav-content2");
 
 menubtn.addEventListener(
   "click",
   () => {
     navigate.classList.toggle("respond");
+  },
+  false
+);
+subnav.addEventListener(
+  "mouseover",
+  (event) => {
+    // console.table(subnavC);
+    subnavC1.classList.toggle("none");
+    subnavC2.classList.toggle("none");
+
+    setTimeout(event => {
+      subnavC1.classList.toggle("none");
+    subnavC2.classList.toggle("none");
+
+    }, 10000);
+    // subnavC.forEach(sub => {
+    // sub.classList.toggle("none");
+    // })
   },
   false
 );
